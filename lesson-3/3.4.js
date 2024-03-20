6e3d53396bc2f53676b3a1bb97fea5e0b020fa47
zlddydy <75608752+zlddydy@users.noreply.github.com>
2024-03-20 04:41:53 +0000

    3끝!

--- lesson-3/3.4.js
+++ lesson-3/3.4.js
@@ -12,6 +12,15 @@
  */
 
 // 먼저 터미널에서 npm install cities 명령해야 한다.
-const cities; // US addresses only (미국 주소만 가능)
+const cities = require('cities');
 
-var myCity;
+var myCity = cities.zip_lookup("82901");
+console.log(myCity);
+
+//82901 = Rock Springs, WY
+//90201 = Beverly Hills, San Francisco, CA
+//60606 = Chicago, IL
+//99501 = Anchorage, AK
+//33162 = Miami, FL
+//10007 = NYC, NY
+//77070 = Houston, TX
