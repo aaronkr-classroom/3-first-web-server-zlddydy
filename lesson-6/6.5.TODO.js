19641a75e54741186378bb3c5b0dd1559bf27442
zlddydy <75608752+zlddydy@users.noreply.github.com>
2024-03-27 05:03:53 +0000

    6끝!

--- lesson-6/6.5.TODO.js
+++ lesson-6/6.5.TODO.js
@@ -6,18 +6,42 @@
  *
  * (이 router.js 파일을 listing6.6js (main.js)에서 require로 사용해야 한다)
  */
-const httpStatus,
-  htmlContentType,
+const httpStatus = require('http-status-codes'),
+  htmlContentType = {
+    "Content-Type": "text/html"
+  },
   // POST 및 GET 요청에 매핑된 라우트를 저장할 routes 객체의 정의
-  routes;
+  routes = {
+    GET: {
+      "/info": (req, res) => {
+        res.writeHead(httpStatus.OK, htmlContentType)
+        res.end("Welcome to the INFO page!")
+      }
+    },
+    POST: {}
+  };
 
 // 라우트에 따른 콜백 함수를 처리하기 위한 함수 handle의 생성
-exports.handle;
+exports.handle = (req, res) => {
+  try{
+    if (routes[req.method][req.url]) {
+      routes[req.method][req.url](req,res);
+    } else{
+      res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
+      res.end("<h1>404</h1><h2>File not found.</h2>");
+    }
+  } catch(e) {
+    console.log("error: " + e);
+  }
+};
 
 // main.js로부터 routes에 등록하기 위한 get 및 post 함수 생성
-exports.get;
-exports.post;
-
+exports.get = (url, action) => {
+  routes["GET"][url] = action;
+};
+exports.post = (url, action) => {
+  routes["POST"][url] = action;
+};
 // <<< 나머진 라우트 코드 입력 하십시오 >>>
 
 /**
--- lesson-6/6.6.TODO.js
+++ lesson-6/6.6.TODO.js
@@ -6,23 +6,45 @@
  *
  * (이 것은 main.js 파일에서 listing6.5.js (router.js) 파일을 require로 필요한다)
  */
-const port,
-  http,
-  httpStatusCodes,
-  router, // listing6.5.js을 필요한다
-  fs,
-  plainTextContentType,
-  htmlContentType,
+const port        = 3000,
+  http            = require('http'),
+  httpStatusCodes = require('http-status-codes'),
+  router          = require('./6.5.TODO'), // listing6.5.js을 필요한다
+  fs              = require('fs'),
+  plainTextContentType = {
+    "Content-Type" : "text/plain"
+  },
+  htmlContentType = {
+    "Content-Type" : "text/html"
+  },
   // 코드 반복을 줄이기 위한 변경된 readFile 함수의 생성
-  customReadFile;
+  customReadFile = (file_path, res) => {
+    fs.readFile(`./${file_path}`, (err,data) => {
+      if (err) console.log("Error reading file: " + err);
+      res.end(data);
+    })
+  };
 
 // get과 post로 라우트 등록
-router.get; // '/'
+router.get("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("INDEX");
+}); // '/'
 
-router.get; // '/index.html'
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  customReadFile("./views/index.html", res);
+}); // '/index.html'
 
-router.post; // '/'
+router.post("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  res.end("POSTED!!");
+}); // '/'
 
 // router.js를 통한 모든 요청 처리
+const app = http.createServer(router.handle);
+if (process.env.NODE_ENV !== "test")
+ app.listen(port);
+console.log(`Server at: http//localhost:${port}`);
 
-console.log();
+module.exports = app;
